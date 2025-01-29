1eaa8822cf8cbfd9054ce3ea5a76cbc24839f31d
Rian Sousa <riansousa666@gmail.com>
2025-01-29 09:04:47 -0300

    ex02 renomeado

---
+++ ex02_Employee_Heranca_E_Polimorfismo/src/application/Program.java
@@ -0,0 +1,13 @@
+package application;
+
+import entities.Employee;
+
+public class Program {
+
+	public static void main(String[] args) {
+		Employee emp = new Employee("Rian", 6, 20.00);
+
+		System.out.println(emp);
+	}
+
+}
